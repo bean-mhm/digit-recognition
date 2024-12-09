1b7e9651930149282d8a293e7a02c006bfcfe2a3
bean <harry.bean.dev@gmail.com>
2024-12-09 20:47:47 +0000

    AppDigitRecognition now reads the training and test images and labels

--- digit-recognition/src/app_digit_recognition.cpp
+++ digit-recognition/src/app_digit_recognition.cpp
@@ -8,10 +8,65 @@ namespace digitrec
 
     void AppDigitRecognition::run()
     {
-        load_digit_samples();
+        load_digit_samples(TRAIN_IMAGES_PATH, TRAIN_LABELS_PATH, train_samples);
+        load_digit_samples(TEST_IMAGES_PATH, TEST_LABELS_PATH, test_samples);
     }
 
-    void AppDigitRecognition::load_digit_samples()
-    {}
+    void AppDigitRecognition::load_digit_samples(
+        std::string_view images_path,
+        std::string_view labels_path,
+        std::vector<DigitSample>& out_samples
+    )
+    {
+        auto stream_images = stream::open_binary_file(images_path);
+        auto stream_labels = stream::open_binary_file(labels_path);
+
+        int32_t magic_images = stream::read_bigend<int32_t>(stream_images);
+        if (magic_images != 2051u)
+        {
+            throw std::runtime_error(
+                "invalid magic number, make sure your files aren't corrupted"
+            );
+        }
+
+        int32_t magic_labels = stream::read_bigend<int32_t>(stream_labels);
+        if (magic_labels != 2049u)
+        {
+            throw std::runtime_error(
+                "invalid magic number, make sure your files aren't corrupted"
+            );
+        }
+
+        int32_t n_items = stream::read_bigend<int32_t>(stream_images);
+        int32_t n_items_labels = stream::read_bigend<int32_t>(stream_labels);
+        if (n_items != n_items_labels)
+        {
+            throw std::runtime_error(
+                "item counts don't match in images and labels"
+            );
+        }
+
+        int32_t image_width = stream::read_bigend<int32_t>(stream_images);
+        int32_t image_height = stream::read_bigend<int32_t>(stream_images);
+        if (image_width != DIGIT_WIDTH || image_height != DIGIT_HEIGHT)
+        {
+            throw std::runtime_error(std::format(
+                "invalid image dimensions {}x{}, expected {}x{}",
+                image_width, image_height, DIGIT_WIDTH, DIGIT_HEIGHT
+            ));
+        }
+
+        size_t prev_size = out_samples.size();
+        out_samples.resize(prev_size + n_items);
+        for (size_t i = prev_size; i < out_samples.size(); i++)
+        {
+            stream::read<uint8_t>(
+                stream_images,
+                out_samples[i].values.data(),
+                N_DIGIT_VALUES
+            );
+            out_samples[i].label = stream::read<uint8_t>(stream_labels);
+        }
+    }
 
 }
--- digit-recognition/src/app_digit_recognition.hpp
+++ digit-recognition/src/app_digit_recognition.hpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 #include <string>
 #include <format>
 #include <array>
@@ -14,6 +15,8 @@
 #include <cstdint>
 
 #include "neural.hpp"
+#include "endian.hpp"
+#include "stream.hpp"
 
 namespace digitrec
 {
@@ -22,10 +25,17 @@ namespace digitrec
     static constexpr size_t DIGIT_HEIGHT = 28;
     static constexpr size_t N_DIGIT_VALUES = DIGIT_WIDTH * DIGIT_HEIGHT;
 
+    static constexpr auto TRAIN_IMAGES_PATH =
+        "./MNIST/train-images.idx3-ubyte";
+    static constexpr auto TRAIN_LABELS_PATH =
+        "./MNIST/train-labels.idx1-ubyte";
+    static constexpr auto TEST_IMAGES_PATH = "./MNIST/t10k-images.idx3-ubyte";
+    static constexpr auto TEST_LABELS_PATH = "./MNIST/t10k-labels.idx1-ubyte";
+
     struct DigitSample
     {
         // pixel values for a digit stored in a row major format
-        std::array<float, N_DIGIT_VALUES> values;
+        std::array<uint8_t, N_DIGIT_VALUES> values;
 
         // digit label from 0 to 9. I didn't use uint8_t for better alignment.
         uint32_t label;
@@ -39,10 +49,14 @@ namespace digitrec
         void run();
 
     private:
-        std::vector<DigitSample> training_samples;
+        std::vector<DigitSample> train_samples;
         std::vector<DigitSample> test_samples;
 
-        void load_digit_samples();
+        void load_digit_samples(
+            std::string_view images_path,
+            std::string_view labels_path,
+            std::vector<DigitSample>& out_samples
+        );
 
     };
 
